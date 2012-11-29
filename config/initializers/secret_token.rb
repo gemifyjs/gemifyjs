45443fb322afa52f76ff16ae62bc98bd40cc5a7b
Stephen Chen <stephenchen13@gmail.com>
2012-11-29 10:52:58 -0500

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gemifyjs::Application.config.secret_token = '77990c6cf612fa85fd27fbad7bbd797ef6a49e44bb9f08ed8b94b9f7024905b859ffdc7c1819ef5378ccc6b5382b1126b54c175c69f94ead23c89848dbf2001c'
