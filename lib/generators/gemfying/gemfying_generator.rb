96799623e4ac972b7111f0c91c6dc77466b74d47
Stephen Chen <stephenchen13@gmail.com>
2012-11-29 15:24:34 -0500

    generating assets folder, added file argument

--- lib/generators/gemfying/gemfying_generator.rb
+++ lib/generators/gemfying/gemfying_generator.rb
@@ -1,16 +1,22 @@
 class GemfyingGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
-  # argument :jem_name, :type => :string, :default => "none"
-  # argument :opts, :type => :hash, :default => {}
+  argument :file, :type => :string, :required => true
+
+  def source_paths
+    [File.expand_path('../templates', __FILE__), Dir.pwd]
+  end
 
   def generate_layout
+    # create necessary files
     target = File.join(Dir.pwd, "jems/#{file_name}-rails")
     template "engine.rb.erb", File.join(target, "lib/#{file_name}-rails/engine.rb")
     template "version.rb.erb", File.join(target, "lib/#{file_name}-rails/version.rb")
     template "railtie.rb.erb", File.join(target, "lib/#{file_name}-rails/railtie.rb")
-    template "versioncontrol.rb.erb", File.join(target, "lib/#{file_name}-rails.rb")
+    template "railsloader.rb.erb", File.join(target, "lib/#{file_name}-rails.rb")
 
-    # template "testing.html.erb", "#{file_name}-rails/testing.html.erb"
+    # copy provided javascript into this directory
+    FileUtils.mkdir_p(File.join(target, 'vendor/assets/javascripts'))
+    copy_file file, File.join(target, "vendor/assets/javascripts/#{file_name}.js")
   end
 
   private
