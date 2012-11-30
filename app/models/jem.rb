992f99aedb0f37adc2440200388a27e20c9e0046
Stephen Chen <stephenchen13@gmail.com>
2012-11-30 11:39:45 -0500

    added validation to jem name

--- app/models/jem.rb
+++ app/models/jem.rb
@@ -1,5 +1,13 @@
 class Jem < ActiveRecord::Base
   attr_accessible :description, :name, :js
+  
+  before_create :normalize_values
 
   mount_uploader :js, JsUploader
+
+  validates :name, :uniqueness => true
+  
+  def normalize_values
+    self.name.downcase.strip! if self.name
+  end
 end
