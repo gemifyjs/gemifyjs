06ccf924a83f87e5a526e322494dbb2c07ebefb5
Stephen Chen <stephenchen13@gmail.com>
2012-12-02 23:44:36 -0500

    cap deploy support

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,8 @@ role :db,  "96.8.123.99", :primary => true # This is where Rails migrations will
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
-
+after "deploy:finalize_update", "config:github"
+after "deploy:finalize_update", "config.rubygems"
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
@@ -28,5 +29,15 @@ namespace :deploy do
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+namespace :config do
+  task :github, :roles => :app do
+    run "cp -f ~/github.yml #{release_path}/config/github.yml"
+  end
+
+  task :rubygems, :roles => :app do
+    run "cp -f ~/rubygems.yml #{release_path}/config/rubygems.yml"
   end
 end
