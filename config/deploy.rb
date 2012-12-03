394cf14f3e65080453a08b10938335a70b7e3afd
Stephen Chen <stephenchen13@gmail.com>
2012-12-02 23:26:58 -0500

    added capistrano for deployment

---
+++ config/deploy.rb
@@ -0,0 +1,32 @@
+require 'bundler/capistrano'
+
+set :application, "gemifyjs"
+set :repository,  "git@github.com:gemifyjs/gemifyjs.git"
+
+set :user, 'gemifyjs'
+set :deploy_to, "/home/#{user}/#{application}"
+set :use_sudo, false
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+default_run_options[:pty] = true
+
+role :web, "96.8.123.99"                          # Your HTTP server, Apache/etc
+role :app, "96.8.123.99"                          # This may be the same as your `Web` server
+role :db,  "96.8.123.99", :primary => true # This is where Rails migrations will run
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
