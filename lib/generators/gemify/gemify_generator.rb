6a2107e6e6d5bf68689c2fba1040280e487e706b
Stephen Chen <stephenchen13@gmail.com>
2012-12-26 01:51:32 -0500

    added logging for debugging

--- lib/generators/gemify/gemify_generator.rb
+++ lib/generators/gemify/gemify_generator.rb
@@ -2,10 +2,12 @@ class GemifyGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
 
   def source_paths
+    logger.debug "source_paths"
     [File.expand_path('../templates', __FILE__), Dir.pwd]
   end
 
   def generate_files
+    logger.debug "generate_files"
     # create necessary files
     target = File.join(Dir.pwd, "jems/#{jem.name}")
     template "engine.rb.erb", File.join(target, "lib/#{jem.name}/engine.rb")
@@ -15,8 +17,11 @@ class GemifyGenerator < Rails::Generators::NamedBase
     template "gemspec.rb.erb", File.join(target, "#{jem.name}.gemspec")
     template "README.md.tt", File.join(target, "README.md")
 
+    logger.debug "create repo"
     repo = GithubService.create_repository(jem.name)
+    logger.debug "add collab"
     GithubService.add_collaborator(repo.full_name, jem.creator.login)
+    logger.debug "push repo"
     GithubService.push_local_repo("jems/#{jem.name}", repo.ssh_url)
 
     Dir.chdir(target) do
